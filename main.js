d3cec7f2ba5ea678cd82f00af0357db04e8b6e12
Leonelta <ketchakaren@gmail.com>
2020-12-05 11:30:43 +0100

    nav bar
    styling the nav bar

---
+++ main.js
@@ -0,0 +1,7 @@
+//select element function
+const selectElement = function (element) {
+    return document.querySelector(element);
+};
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
